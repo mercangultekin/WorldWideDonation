4d35d1274a9b704231e4f8981fecc2544f49f14d
Muhammed Ali <malialtinel@gmail.com>
2017-12-15 22:48:28 +0300

    Controller for Searching

---
+++ SearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WWD.Models;
+
+namespace WWD.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        DonationDB ctx = new DonationDB();
+        public ActionResult Index()
+        {
+         
+           ViewBag.HAYDA = TempData["haydarhaydar"];
+            return View();
+        }
+        public ActionResult Arama(string haydar)
+        {
+           List<Kampanya> haydari = ctx.Kampanyas.Where(x => x.Adi.Contains(haydar)||x.Kategori.KategoriAdi.Contains(haydar)).ToList();
+            TempData["haydarhaydar"] = haydari;
+            return RedirectToAction("Index");
+        }
+
+    }
+}
