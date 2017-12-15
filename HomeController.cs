43768ea24c30449991fa07fa8d9c31c7b95c822c
Muhammed Ali <malialtinel@gmail.com>
2017-12-15 22:47:43 +0300

    Controller for Main Page

---
+++ HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WWD.Models;
+namespace WWD.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DonationDB ctx = new DonationDB();
+        public ActionResult Index()
+        {
+            var ca = ctx.Kampanyas.Where(x => x.OnaylandiMi == true).ToList();
+            return View(ca);
+        }
+    }
+}
